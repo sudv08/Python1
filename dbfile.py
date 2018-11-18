d0cae6ca8fb2b4ede634b9a967851109c85c8b49
sudv08 <sudeepvyas98@gmail.com>
2018-11-18 12:35:39 +0530

    class file function, intendentation

--- dbfile.py
+++ dbfile.py
@@ -8,24 +8,23 @@ following things will cover today
 
 
 
-"""
+
 a=int(input("enter 1st number:"))
 b=int(input("enter 2nd number:"))
 c=int(input("enter 3rd number:"))
 d=int(input("enter 4rth number:"))
 e=int(input("enter 5th number:"))
 if  (a>b) and (a>c) and (a>d) and (a>e):
-        print ("largest numner:", a)
+        print ("largest number:", a)
 elif (b>a) and (b>c) and (b>d) and (b>e):
-        print ("largest numner:", b)
+        print ("largest number:", b)
 elif (c>a) and (c>b) and (c>d) and (c>e):
-        print ("largest numner:", c)
+        print ("largest number:", c)
 elif (d>a) and (d>b) and (d>c) and (d>e):
-        print ("largest numner:", d)
+        print ("largest number:", d)
 elif (e>a) and (e>b) and (e>c) and (e>d):
-        print ("largest numner:", e)
+        print ("largest number:", e)
 
-"""
 
 
 #for i in range(0,10,2):
@@ -43,20 +42,21 @@ for i in range(0, 5):
 
 """
 
-k = 0
-rows = 5
-for i in range(1, rows):
-    for space in range(1, (rows)+1):
-        print(end="  ")
-    while k != i : #(2*i-1):
-        print("* ", end="")
-        k = k + 1
-    k = 0
-    print()
+# k = 0
+# rows = 5
+# for i in range(1, rows):
+#     for space in range(1, (rows)+1):
+#         print(end="  ")
+#     while k != i : #(2*i-1):
+#         print("* ", end="")
+#         k = k + 1
+#     k = 0
+#     print()
 
 """
-
+"""
 x = {
     'name': 'sudeep'
     'name': 'pune'
 }
+"""

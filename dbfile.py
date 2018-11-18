5483e21f2cace1b67f2395a4c3148a95e25234ac
sudv08 <sudeepvyas98@gmail.com>
2018-11-18 11:25:14 +0530

    commit it

---
+++ dbfile.py
@@ -0,0 +1,62 @@
+""""
+following things will cover today
+
+"""
+
+
+# print(list().__dir__())
+
+
+
+"""
+a=int(input("enter 1st number:"))
+b=int(input("enter 2nd number:"))
+c=int(input("enter 3rd number:"))
+d=int(input("enter 4rth number:"))
+e=int(input("enter 5th number:"))
+if  (a>b) and (a>c) and (a>d) and (a>e):
+        print ("largest numner:", a)
+elif (b>a) and (b>c) and (b>d) and (b>e):
+        print ("largest numner:", b)
+elif (c>a) and (c>b) and (c>d) and (c>e):
+        print ("largest numner:", c)
+elif (d>a) and (d>b) and (d>c) and (d>e):
+        print ("largest numner:", d)
+elif (e>a) and (e>b) and (e>c) and (e>d):
+        print ("largest numner:", e)
+
+"""
+
+
+#for i in range(0,10,2):
+   # print(i)
+
+#k = 5
+for i in range(0, 5):
+    for j in range(0,5):
+        print("* ", end="")
+   # k = k + 1
+    print()
+
+
+
+
+"""
+
+k = 0
+rows = 5
+for i in range(1, rows):
+    for space in range(1, (rows)+1):
+        print(end="  ")
+    while k != i : #(2*i-1):
+        print("* ", end="")
+        k = k + 1
+    k = 0
+    print()
+
+"""
+
+x = {
+    'name': 'sudeep'
+    'name': 'pune'
+}
